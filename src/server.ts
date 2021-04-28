cc0f24fcb164867d160fe9a5482c1d65c8c0fd2a
Mikkel Christensen <mikch@protonmail.com>
2021-04-28 10:23:46 +0200

    minimal setup with TS and Express

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+﻿import {Express, Request, Response} from 'express';
+const app: Express = require('express')();
+
+const PORT: number = 8000;
+
+app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server'));
+
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at https://localhost:${PORT}`);
+});
